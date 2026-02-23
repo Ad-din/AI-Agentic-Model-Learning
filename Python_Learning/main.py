059d2d2c4f1e0adcc10c7a37132a1c94fa837c1b
Ad-din <mda126044@gmail.com>
2026-02-24 03:31:11 +0600

    first lesson done!

--- Python_Learning/main.py
+++ Python_Learning/main.py
@@ -1,6 +1,27 @@
-name=input("What is your name?")
-age=input("How old are you")
+#Staring from here to learn:
+#input()= A funciton that prompts the user to enter data return the entered data as a string
 
-print(f"Hello {name}")
-print(f"You are {age} years old")
+
+# name=input("What is your name?")
+# age=input("How old are you")
+
+# print(f"Hello {name}")
+# print(f"You are {age} years old")
+
+# #type casting:
+
+# age= int(age)
+# age=1+age
+# print(age)
+
+# #Or type caste it while receiving the input. Like:
+
+# age=int(input("Enter you age:"))
+# print(age+1)
+
+# item=input("What itme would you like to buy?")
+# price=float(input("What is the price:"))
+# quantity=int(input("how many would you like?"))
+# total=price*quantity
+# print(total)
 
