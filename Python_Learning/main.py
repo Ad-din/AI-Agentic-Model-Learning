614aaf9d35b92948140abf685263de782c7c5de0
Ad-din <mda126044@gmail.com>
2026-02-24 11:39:46 +0600

    upto statements

--- Python_Learning/main.py
+++ Python_Learning/main.py
@@ -25,3 +25,31 @@
 # total=price*quantity
 # print(total)
 
+# friends=1
+# friends+=friends
+# print(friends)
+
+#IF-else operator
+
+operator= input("Enter an operator( + - * /): ")
+num1= float(input("Enter the 1st number:"))
+num2= float(input("Enter the 2nd number:"))
+
+
+if operator == "+":
+    result=num1+num2
+    print(result)
+elif operator == "-":
+    result=num1-num2
+    print(result)
+elif operator == "*":
+    result=num1*num2
+    print(result)
+elif operator == "/":
+    if num2==0:
+        print("Cannot be divided by zero")
+        pass
+    else:
+        result=num1/num2
+        print(result)
+
